e965f9250313ac327a3a92eedd8fe0697b87574d
allenabraham777 <allenabraham777@gmail.com>
2022-12-21 17:54:36 +0000

    Build: (32869c1) Update index to server static files

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ var _express = _interopRequireDefault(require("express"));
 var _morgan = _interopRequireDefault(require("morgan"));
 var _cors = _interopRequireDefault(require("cors"));
 var _httpErrors = _interopRequireDefault(require("http-errors"));
+var _path = _interopRequireDefault(require("path"));
 var _middlewares = require("./middlewares");
 var _connection = _interopRequireDefault(require("./database/connection"));
 var _routes = require("./routes");
@@ -25,6 +26,19 @@ app.use(_express["default"].urlencoded({
 (0, _routes.chatRoutes)(app);
 (0, _routes.messageRoutes)(app);
 (0, _routes.notificationRoutes)(app);
+var dirname = _path["default"].resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(_express["default"]["static"](_path["default"].join(dirname, "frontend/dist")));
+  app.get("*", function (req, res) {
+    res.sendFile(_path["default"].resolve(dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", function (req, res) {
+    res.json({
+      message: "API running successfully"
+    });
+  });
+}
 app.use(function (req, res, next) {
   next((0, _httpErrors["default"])(404));
 });
