32869c1c3438693436dbffc6f7c50987d7c09aca
Allen K Abraham <allenkabraham@Allens-MacBook-Pro.local>
2022-12-21 23:23:56 +0530

    Update index to server static files

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import createError from "http-errors";
+import path from "path"
 
 import { errorHandler } from "middlewares";
 import connectDB from "database/connection";
@@ -27,6 +28,18 @@ uploadRoutes(app);
 chatRoutes(app);
 messageRoutes(app);
 notificationRoutes(app);
+
+const dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "API running successfully" });
+  });
+}
 
 app.use((req, res, next) => {
   next(createError(404));
